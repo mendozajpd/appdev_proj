a2d63bae66972ef458bac7b9e7bcb24dc29b69f2
mendozajpd <98361827+mendozajpd@users.noreply.github.com>
2024-05-08 04:25:50 +0800

    now functional playing view

--- react/src/js/UserPlayingView.js
+++ react/src/js/UserPlayingView.js
@@ -35,13 +35,14 @@ const UserPlayingView = () => {
                 </div>
                 <CloseButton variant="white" onClick={() => { setPlayingViewActive(false) }} />
             </div>
-            <div className="py-5 justify-content-center">
-                <Image src="https://via.placeholder.com/400x250" alt="Album Cover" style={{ width: '100%', height: '100%' }} />
-            </div>
-            {songDetails && <>
+            {songDetails ? <>
+                <div className="py-5 justify-content-center">
+                    {/* <Image src="https://via.placeholder.com/400x250" alt="Album Cover" style={{ width: '100%', height: '100%' }} /> */}
+                    <Image src={`${BACKEND_URL}/storage/album_images/${songDetails.album.cover_photo_hash}`} alt="Album Cover" style={{ width: '100%', height: '100%' }} />
+                </div>
                 <div className="d-flex justify-content-between py-3">
                     <div className="">
-                        <div className="display-7 text-truncate" style={{maxWidth:'15rem'}}>
+                        <div className="display-7 text-truncate" style={{ maxWidth: '15rem' }}>
                             {songDetails.display_name}
                         </div>
                         <div className="text-gray">
@@ -63,7 +64,7 @@ const UserPlayingView = () => {
                     <div className="d-flex justify-content-between">
                         <div className="py-3 d-flex gap-2">
                             <div>
-                                <Image src="https://via.placeholder.com/50x50" alt="Artist" rounded />
+                                <Image src={`${BACKEND_URL}/storage/album_images/${songDetails.album.cover_photo_hash}`} alt="Album Cover" style={{ width: '50px', height: '50px' }} rounded />
                             </div>
                             <div>
                                 <div>
@@ -87,6 +88,14 @@ const UserPlayingView = () => {
 
                 </div>
             </>
+                : (
+                    <div className="h-50  align-items-center justify-content-center d-flex flex-column text-gray">
+                        <i className="fa fa-headphones display-1" />
+                        <div className="py-3">
+                            Wow this looks empty. Play something!
+                        </div>
+                    </div>
+                )
             }
 
         </div>
